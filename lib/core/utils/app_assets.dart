4cfd8d1d9735651c6fe5230f57522b97a5f43af2
Laila Ragab <lailaragab144@gmail.com>
2025-12-02 16:30:55 +0200

    Finish app assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -21,21 +21,16 @@ class AppAssets {
   static String successScan = "${SvgImages.svgPath}success_scan.svg";
   static String errorScan = "${SvgImages.svgPath}error_scan.svg";
   static String home = "${SvgImages.svgPath}home.svg";
-  static String logOut = "${SvgImages.svgPath}log_out.svg";
-  static String bill = "${SvgImages.svgPath}bill.svg";
-  static String languages = "${SvgImages.svgPath}language.svg";
-  static String errorAlarm="${SvgImages.svgPath}errorAlarm.svg";
+  static String errorAlarm = "${SvgImages.svgPath}errorAlarm.svg";
 
   static String prods = "${SvgImages.svgPath}prods.svg";
   static String company = "${SvgImages.svgPath}company.svg";
   static String download = "${SvgImages.svgPath}download.svg";
   static String settings = "${SvgImages.svgPath}settings.svg";
-  static String profile = "${SvgImages.svgPath}profile.svg";
   static String visits = "${SvgImages.svgPath}visits.svg";
   static String success = "${SvgImages.svgPath}dialog_success.svg";
   static String failure = "${SvgImages.svgPath}dialog_failure.svg";
   static String arrowBack = "${SvgImages.svgPath}arrow_back.svg";
-  static String arrowLeft = "${SvgImages.svgPath}arrow_left.svg";
   static String eyeIcon = "${SvgImages.svgPath}eye.svg";
   static String eyeOffIcon = "${SvgImages.svgPath}eye_off.svg";
   static String arrow = "${SvgImages.svgPath}arrow.svg";
@@ -74,6 +69,30 @@ class AppAssets {
   static String cameraFrame = "${SvgImages.svgPath}camera_frame.svg";
   static String checkCircle = "${SvgImages.svgPath}check_circle.svg";
 
+  //....................XDeal.................................................
+  static String profile = "${SvgImages.svgPath}profile.svg";
+  static String bill = "${SvgImages.svgPath}bill.svg";
+  static String darkTheme = "${SvgImages.svgPath}dark_theme.svg";
+  static String languages = "${SvgImages.svgPath}language.svg";
+  static String repeatedQuestions =
+      "${SvgImages.svgPath}repeated_questions.svg";
+  static String support = "${SvgImages.svgPath}support.svg";
+  static String termsAndConditions =
+      "${SvgImages.svgPath}terms_and_conditions.svg";
+  static String deleteAccount = "${SvgImages.svgPath}delete_account.svg";
+  static String privacyPolicy = "${SvgImages.svgPath}privacy_policy.svg";
+  static String logOut = "${SvgImages.svgPath}log_out.svg";
+  static String filledCheckBox = "${SvgImages.svgPath}filled_check_box.svg";
+  static String call = "${SvgImages.svgPath}call.svg";
+  static String arrowDown = "${SvgImages.svgPath}arrow_down.svg";
+  static String share = "${SvgImages.svgPath}share.svg";
+  static String arrowLeft = "${SvgImages.svgPath}arrow_left.svg";
+  static String starFilled = "${SvgImages.svgPath}star_filled.svg";
+  static String star = "${SvgImages.svgPath}star.svg";
+  static String fav = "${SvgImages.svgPath}fav.svg";
+  static String notification = "${SvgImages.svgPath}notification.svg";
+  static String cilantroLogo = "${SvgImages.svgPath}cilantro_logo.svg";
+  static String elabdLogo = "${SvgImages.svgPath}elabd_logo.svg";
   //! PNG
 
   static String logo = "${PngImages.imagesPath}logo.png";
@@ -84,5 +103,23 @@ class AppAssets {
   static String onboarding3 = "${PngImages.imagesPath}onboarding3.png";
   static String english = "${PngImages.imagesPath}english.png";
   static String arabic = "${PngImages.imagesPath}arabic.png";
+  static String searchNoOffers = "${PngImages.imagesPath}search_no_offers.png";
+  static String searchFailed = "${PngImages.imagesPath}search_failed.png";
 
+  //....................................XDeal................................................
+  static String message = "${PngImages.imagesPath}message.png";
+  static String vodafoneCash = "${PngImages.imagesPath}vodafone_cash.png";
+  static String eAndMoney = "${PngImages.imagesPath}e_and_money.png";
+  static String orangeCash = "${PngImages.imagesPath}orange_cash.png";
+  static String fawry = "${PngImages.imagesPath}fawry.png";
+  static String masterCard = "${PngImages.imagesPath}master_card.png";
+  static String vodafoneCashHeader =
+      "${PngImages.imagesPath}vodafone_cash_header.png";
+  static String barcode = "${PngImages.imagesPath}barcode.png";
+  static String qrCode = "${PngImages.imagesPath}qr_code.png";
+  static String paper = "${PngImages.imagesPath}paper.png";
+  static String offerMessage = "${PngImages.imagesPath}offer_message.png";
+  static String elabd1 = "${PngImages.imagesPath}elabd1.png";
+  static String cilantro1 = "${PngImages.imagesPath}cilantro1.png";
+  static String billFilled = "${PngImages.imagesPath}bill_filled.png";
 }
